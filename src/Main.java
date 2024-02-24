9e92bd9b8097fd70296718d2d47ab23bcaada3d0
remix <http+github.com@remix.asia>
2024-02-24 10:47:24 +0900

    abort [Enter], use input Thread.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 public class Main{
 	public static void main( String[] args ){
+		ThreadA t = new ThreadA();
+		t.start();
+
 		BufferedReader br= null;
 		try{
 			br= new BufferedReader( new FileReader( new File( "starwars.txt" ) ) );
 			String s;
-			while( null != ( s= br.readLine() ) ){
-				for( int i= 0; i < s.length(); i++ ){
+			while( t.isOn  && null != ( s= br.readLine() ) ){
+				for( int i= 0; t.isOn  && i < s.length(); i++ ){
 					System.out.print( s.charAt( i ) );
 					Thread.sleep( 10 );
 				}
@@ -18,6 +22,7 @@ public class Main{
 		}catch( Exception e ){//readLine(),sleep()
 			e.printStackTrace();
 		}finally{
+			t.isOn= false;
 			if( null != br ){
 				try{
 					br.close();
@@ -25,6 +30,28 @@ public class Main{
 					e.printStackTrace();
 				}
 			}
+		}
+	}
+
+	public static class ThreadA extends Thread{
+		boolean isOn= true;
+
+		public void run(){
+			System.out.println( "=== THREAD START ===" );
+			InputStreamReader br = new InputStreamReader( System.in );
+			while( isOn ){
+				try{
+					if( ! br.ready() ) continue;
+					int i= br.read();//int i= System.in.read();
+				//	if( 'q' == (char)i ){
+						System.out.println( "\naborted" );
+						isOn= false;
+				//	}
+				}catch( Exception e ){
+					e.printStackTrace();
+				}
+			}
+			System.out.println( "=== THREAD END ===" );
 		}
 	}
 }
